8cf466d5c29ad0a5ebc759a4774b9f775bf6d064
@DaveBlakeman <pdb@rpsys.co.uk>
2019-09-25 22:00:44 +0100

    Update IslandTest2.js

--- IslandTest2.js
+++ IslandTest2.js
@@ -1,33 +1,7 @@
-// Core, Team, and Official extensions can `require` VM code:
-const ArgumentType = require('../../extension-support/argument-type');
-const BlockType = require('../../extension-support/block-type');
-const TargetType = require('../../extension-support/target-type');
-
-// ...or VM dependencies:
-const formatMessage = require('format-message');
-
-// Core, Team, and Official extension classes should be registered statically with the Extension Manager.
-// See: scratch-vm/src/extension-support/extension-manager.js
-
-
-
 class IslandTest {
-    constructor (runtime) {
-        /**
-         * Store this for later communication with the Scratch VM runtime.
-         * If this extension is running in a sandbox then `runtime` is an async proxy object.
-         * @type {Runtime}
-         */
-        this.runtime = runtime;
-    }
 
-    /**
-     * @return {object} This extension's metadata.
-     */
     getInfo () {
         return {
-            // Required: the machine-readable name of this extension.
-            // Will be used as the extension's namespace.
             "id": "IslandTest",
             "name": "IslandTest",
             "blocks": [
@@ -39,19 +13,9 @@ class IslandTest {
                 }
             ]
         };
-    };
+    }
 
-    /**
-     * Implement clear_all_scores.
-     * @param {object} args - the block's arguments.
-     * @property {string} MY_ARG - the string value of the argument.
-     * @returns {string} a string which includes the block argument value.
-     */
     clear_all_scores () {
-		/*var players = [];
-		for (playerIndex = 0; playerIndex < players.length; playerIndex++) {
-		  players[playerIndex].score = 0;
-		}*/
     }
 }
 
