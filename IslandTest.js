0b688a7ea5d3a41aa64c98d044f1e569d7846a0d
@DaveBlakeman <pdb@rpsys.co.uk>
2019-09-25 10:18:19 +0100

    Update IslandTest.js

--- IslandTest.js
+++ IslandTest.js
@@ -95,8 +95,8 @@ var players = [];
             ['r', 'get player %n costume', 'get_player_costume', 1], 
             ['r', 'get player %n score', 'get_player_score', 1] , 
             [' ', 'change player %n score by %n', 'change_player_score', 1, 1], 
-            [' ', 'set player %n name', 'set_player_name', 1], 
-            [' ', 'set player %n costume', 'set_player_costume', 1], 
+            [' ', 'set player %n name to %s', 'set_player_name', 1, 'name'], 
+            [' ', 'set player %n costume to %s', 'set_player_costume', 1, 'costume'], 
             [' ', 'add player with name %s ', 'add_player', ''], 
             [' ', 'remove player %n ', 'remove_player', 1] , 
             ['r', 'get index of player %s ', 'get_index_of_player', '']  
