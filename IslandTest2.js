d6040a0580192a6acdc8e03ff30b737b14cb8d93
@DaveBlakeman <pdb@rpsys.co.uk>
2019-09-25 21:23:31 +0100

    Add IslandTest2 - in scratch 3 format

---
+++ IslandTest2.js
@@ -0,0 +1,124 @@
+// Core, Team, and Official extensions can `require` VM code:
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const TargetType = require('../../extension-support/target-type');
+
+// ...or VM dependencies:
+const formatMessage = require('format-message');
+
+// Core, Team, and Official extension classes should be registered statically with the Extension Manager.
+// See: scratch-vm/src/extension-support/extension-manager.js
+
+var players = [];
+
+class IslandTest {
+    constructor (runtime) {
+        /**
+         * Store this for later communication with the Scratch VM runtime.
+         * If this extension is running in a sandbox then `runtime` is an async proxy object.
+         * @type {Runtime}
+         */
+        this.runtime = runtime;
+    }
+
+    /**
+     * @return {object} This extension's metadata.
+     */
+    getInfo () {
+        return {
+            // Required: the machine-readable name of this extension.
+            // Will be used as the extension's namespace.
+            id: 'IslandTest',
+
+            // Core extensions only: override the default extension block colors.
+            color1: '#FF8C1A',
+            color2: '#DB6E00',
+
+            name: formatMessage({
+                id: 'extensionName',
+                defaultMessage: 'Island Test',
+                description: 'The name of the "Island Test" extension'
+            }),
+
+            blockIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+
+            menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAAFCAAAAACyOJm3AAAAFklEQVQYV2P4DwMMEMgAI/+DEUIMBgAEWB7i7uidhAAAAABJRU5ErkJggg==',
+
+            docsURI: 'https://....',
+
+            blocks: [
+                {
+                    opcode: 'clear_all_scores', // becomes 'IslandTest.clear_all_scores'
+                    blockType: BlockType.COMMAND,
+                    branchCount: 0,
+ 
+                    // Required: the human-readable text on this block, including argument
+                    // placeholders. Argument placeholders should be in [MACRO_CASE] and
+                    // must be [ENCLOSED_WITHIN_SQUARE_BRACKETS].
+                    text: formatMessage({
+                        id: 'clear_all_scores',
+                        defaultMessage: 'Clear all scores',
+                        description: 'Label on the "myReporter" block'
+                    }),
+
+                    arguments: {
+                    },
+
+                    filter: [TargetType.SPRITE, TargetType.STAGE]
+                },
+                {
+                    // Another block...
+                }
+            ],
+
+            // Optional: define extension-specific menus here.
+            menus: {
+                // Required: an identifier for this menu, unique within this extension.
+                menuA: [
+                    // Static menu: list items which should appear in the menu.
+                    {
+                        // Required: the value of the menu item when it is chosen.
+                        value: 'itemId1',
+
+                        // Optional: the human-readable label for this item.
+                        // Use `value` as the text if this is absent.
+                        text: formatMessage({
+                            id: 'menuA_item1',
+                            defaultMessage: 'Item One',
+                            description: 'Label for item 1 of menu A in "Some Blocks" extension'
+                        })
+                    },
+
+                    // The simplest form of a list item is a string which will be used as
+                    // both value and text.
+                    'itemId2'
+                ],
+
+                // Dynamic menu: returns an array as above.
+                // Called each time the menu is opened.
+                menuB: 'getItemsForMenuB',
+
+                // The examples above are shorthand for setting only the `items` property in this full form:
+                menuC: {
+                    // This flag makes a "droppable" menu: the menu will allow dropping a reporter in for the input.
+                    acceptReporters: true,
+
+                    // The `item` property may be an array or function name as in previous menu examples.
+                    items: [/*...*/] || 'getItemsForMenuC'
+                }
+            }
+        };
+    };
+
+    /**
+     * Implement clear_all_scores.
+     * @param {object} args - the block's arguments.
+     * @property {string} MY_ARG - the string value of the argument.
+     * @returns {string} a string which includes the block argument value.
+     */
+    clear_all_scores (args) {
+		for (playerIndex = 0; playerIndex < players.length; playerIndex++) {
+		  players[playerIndex].score = 0;
+		}
+    };
+}
