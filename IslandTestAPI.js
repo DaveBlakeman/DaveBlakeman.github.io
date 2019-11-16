ed34a6f76182a872c8a408d8c42485b6e6d888b5
@DaveBlakeman <pdb@rpsys.co.uk>
2019-11-16 14:38:22 +0000

    Update IslandTestAPI.js

--- IslandTestAPI.js
+++ IslandTestAPI.js
@@ -132,25 +132,34 @@ class KillerIsland {
             }
         };
     }
+	
+	init_if_required() {
+		console.log('init')
+	}
 
 	clear_all_scores({}) {
+		init_if_required();
 		for (let player_index = 0; player_index < players.length; player_index++) {
 		  players[player_index].score = 0;
 		}
 	}
 	clear_all_players({}) {
+		init_if_required();
 		players.length = 0;
 	}
 	get_player_count({}) {
+		init_if_required();
 		return players.length;
 	}
 	get_player_name({player_index}) {
+		init_if_required();
 		if ((player_index < 1) || (player_index > players.length))
 		  return "";
 		else  
 		  return players[player_index-1].name;
 	}
 	get_player_costume({player_index}) {
+		init_if_required();
 		if ((player_index < 1) || (player_index > players.length))
 		  return "<null>";
 	    else { 
@@ -159,31 +168,38 @@ class KillerIsland {
 		}
 	}
 	get_player_score({player_index}) {
+		init_if_required();
 		if ((player_index < 1) || (player_index > players.length))
 		  return 0;
 	    else  
 		  return players[player_index-1].score;
 	}
 	change_player_score({player_index, new_score}) {
+		init_if_required();
 		if ((player_index > 0) && (player_index <= players.length))
 		  players[player_index-1].score = new_score;
 	}
 	change_player_name({player_index, new_name}) {
+		init_if_required();
 		if ((player_index > 0) && (player_index <= players.length))
 		  players[player_index-1].name = new_name;
 	}
 	change_player_costume({player_index, new_costume}) {	
+		init_if_required();
 		if ((player_index > 0) && (player_index <= players.length))
 		  players[player_index-1].costume = new_costume;
 	}
 	add_player({player_name}) {
+		init_if_required();
 		players.push(new PlayerRec (player_name, "", 0)); 
 	}  
 	remove_player({player_index}) {
+		init_if_required();
 		if ((player_index > 0) && (player_index <= players.length))
 		   delete players[player_index-1];
 	}   
 	get_index_of_player({player_name}) {
+		init_if_required();
 		for (player_index = 0; player_index < players.length; player_index++) {
 		  if (players[player_index].name == player_name)
 			  return player_index+1; // one-based index
